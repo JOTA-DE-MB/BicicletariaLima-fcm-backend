1b2892a9ae0681b7b1a71f4908d9096e28d8fea3
Joselio Lima <programadorjmb@gmail.com>
2025-06-05 20:01:42 -0300

    correção 2 inicialização do firebase-admin

--- notifications.js
+++ notifications.js
@@ -1,10 +1,8 @@
 if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY);
+
   admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    }),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
